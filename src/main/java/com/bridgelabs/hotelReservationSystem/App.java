3c3e452b4d60f0caf09468e71a624905b2c7e8cd
mukulBillore <mukulbillore88@gmail.com>
2022-01-12 08:44:25 +0530

    Initial added : welcome message

---
+++ src/main/java/com/bridgelabs/hotelReservationSystem/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabs.hotelReservationSystem;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello welcome to hotel Reservation system " );
+    }
+}
