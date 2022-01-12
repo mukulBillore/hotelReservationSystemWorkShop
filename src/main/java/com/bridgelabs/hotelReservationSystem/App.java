3fe1b5ce38078c37607c93606250e6df24d0492c
mukulBillore <mukulbillore88@gmail.com>
2022-01-12 09:09:17 +0530

    added : added the functionality to add hotel

--- src/main/java/com/bridgelabs/hotelReservationSystem/App.java
+++ src/main/java/com/bridgelabs/hotelReservationSystem/App.java
@@ -5,9 +5,18 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello welcome to hotel Reservation system " );
-        HotelClassification hotel1 = new HotelClassification();
-        		hotel1.setNameOfHotel("hotel 1");
-                hotel1.setRateForRegularCust(1);
-                hotel1.setRatingOfHotel(1);
+        HotelClassification lakewood = new HotelClassification();
+        lakewood.setNameOfHotel("Lakewood");
+        lakewood.setRateForRegularCust(110);
+        lakewood.setRatingOfHotel(3);
+        HotelClassification brigewood = new HotelClassification();
+        brigewood.setNameOfHotel("brigewood");
+        brigewood.setRateForRegularCust(160);
+        brigewood.setRatingOfHotel(4);
+        HotelClassification ridgewood = new HotelClassification();
+        ridgewood.setNameOfHotel("Lakewood");
+        ridgewood.setRateForRegularCust(110);
+        ridgewood.setRatingOfHotel(3);
+    
     }
 }
---
+++ src/main/java/com/bridgelabs/hotelReservationSystem/Hotels.java
@@ -0,0 +1,5 @@
+package com.bridgelabs.hotelReservationSystem;
+
+public class Hotels {
+
+}
